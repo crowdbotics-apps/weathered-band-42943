319687aec501f52fda02d956207507c62ccc8801
Crowdbotics <team@crowdbotics.com>
2023-08-19 07:04:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "weathered-band-42943",
+  url: "https://weathered-band-42943.botics.co",
+  api: "https://weathered-band-42943.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
